81276a8498f881dbbb0b1c5334d512989124e190
nivanecu <36429748+nivanecu@users.noreply.github.com>
2024-11-20 15:17:57 -0500

    Reinitializing repository

---
+++ PostPruebasUnitarias/ProcesadorMatematico.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PostPruebasUnitarias
+{
+    public static class ProcesadorMatematico
+    {
+        public static int Sumar(int primerNumero, int segundoNumero)
+        {
+            return primerNumero + segundoNumero;
+        }
+        public static int Resta(int primerNumero, int segundoNumero)
+        {
+            return primerNumero - segundoNumero;
+        }
+        public static int Multiplicacion(int primerNumero, int segundoNumero)
+        {
+            return primerNumero * segundoNumero;
+        }
+        public static int Division(int dividendo, int divisor)
+        {
+            return dividendo / divisor;
+        }
+        public static int Potencia(int numeroBase, int exponente)
+        {
+            return (int)Math.Pow(numeroBase, exponente);
+        }
+        public static int Raiz(int numeroBase, int exponente)
+        {
+            return (int)Math.Pow(numeroBase, 1 / exponente);
+        }
+    }
+}
---
+++ PruebasUnitarias/PruebasProcesador.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PostPruebasUnitarias;
+
+namespace PruebasUnitarias
+{
+    [TestClass]
+    public class PruebasProcesador
+    {
+        [TestMethod]
+        [TestCategory("Suma")]
+        public void TestSumaOK()
+        {
+            //Definimos las variables
+            //Arrange
+            var operador1 = 10;
+            var operador2 = 11;
+
+            //Ejecutamos la prueba
+            //Act
+            var result = ProcesadorMatematico.Sumar(operador1, operador2);
+
+            //Comparamos resultados
+            //Assert
+            var valorEsperado = 21;
+            Assert.AreEqual(valorEsperado, result);
+        }
+
+        [TestMethod]
+        [TestCategory("Suma")]
+        public void TestSumaNOK()
+        {
+            //Definimos las variables
+            //Arrange
+            var operador1 = 0;
+            var operador2 = 0;
+
+            //Ejecutamos la prueba
+            //Act
+            var result = ProcesadorMatematico.Sumar(operador1, operador2);
+
+            //Comparamos resultados
+            //Assert
+            var valorEsperado = 10;
+            Assert.AreNotEqual(valorEsperado, result);
+        }
+
+        [TestMethod]
+        [TestCategory("Division")]
+        public void TestDivision1()
+        {
+            //Definimos las variables
+            //Arrange
+            var dividendo = 10;
+            var divisor = 2;
+
+            //Ejecutamos la prueba
+            //Act
+            var result = ProcesadorMatematico.Division(dividendo, divisor);
+
+            //Comparamos resultados
+            //Assert
+            var valorEsperado = 5;
+            Assert.AreEqual(valorEsperado, result);
+        }
+
+        [TestMethod]
+        [TestCategory("Division")]
+        [ExpectedException(typeof(DivideByZeroException))] //Excepcion esperada
+        public void TestDivisionExcepcion()
+        {
+            //Definimos las variables
+            //Arrange
+            var dividendo = 10;
+            var divisor = 0;
+
+            //Ejecutamos la prueba
+            //Act
+            var result = ProcesadorMatematico.Division(dividendo, divisor);
+
+            //Comparamos resultados
+            //Assert
+            //En este caso, no deberiamos llegar aqui, la prueba correcta es una excepcion de DivisionEntreCero
+        }
+    }
+}
